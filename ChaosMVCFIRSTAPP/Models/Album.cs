d062bbbd278489397b5f426b1b49712b51820f96
DannyRusnok <daniel.rusnok@gmail.com>
2017-01-28 18:52:25 +0100

    ucim se MVC.

---
+++ ChaosMVCFIRSTAPP/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChaosMVCFIRSTAPP.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+
+        public List<Review> Review { get; set; }
+    }
+}
